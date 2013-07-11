d4a1e06eccaea59c089fb7924ed7e430543a75b5
John Smith <example@stanford.edu>
2013-07-11 00:19:17 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("index.html", "utf8");
+
+//var buf = new Buffer(data.length);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
